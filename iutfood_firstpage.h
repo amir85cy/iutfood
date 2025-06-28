4476ef82255b5bfcde78b8f136d802f8ffeb4fc3
amir <amcyirbery85@gmail.com>
2025-06-28 11:13:55 +0330

    creating firstpage classes

---
+++ iutfood_firstpage.h
@@ -0,0 +1,24 @@
+#ifndef IUTFOOD_FIRSTPAGE_H
+#define IUTFOOD_FIRSTPAGE_H
+
+#include <QMainWindow>
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class IUTFood;
+}
+QT_END_NAMESPACE
+
+class IUTFood : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    IUTFood(QWidget *parent = nullptr);
+    ~IUTFood();
+
+private:
+    Ui::IUTFood *ui;
+};
+#endif // IUTFOOD_FIRSTPAGE_H
